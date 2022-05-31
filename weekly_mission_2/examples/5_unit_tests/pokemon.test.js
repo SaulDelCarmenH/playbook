6cbcd8154834cdb53ed2b290214a92f39ce8f5ec
Saul Del Carmen <saul.delcarmenhernandez@gmail.com>
2022-05-30 20:12:00 -0500

    fix test

--- weekly_mission_2/examples/5_unit_tests/pokemon.test.js
+++ weekly_mission_2/examples/5_unit_tests/pokemon.test.js
@@ -4,3 +4,8 @@ test('1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Pikachu')
   expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
 });
+
+test('2) Create a new object pokemon', () => {
+  const myPokemon = new Pokemon('Pikachu')
+  expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
+});
