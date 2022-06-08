9ca990f38dee69093766ab28ea839d2c81297dac
Saul Del Carmen <saul.delcarmenhernandez@gmail.com>
2022-06-08 15:25:33 -0500

    add express_basic

---
+++ weekly_mission_3/express_basic/app.js
@@ -0,0 +1,36 @@
+// Usando objeto express
+const express = require('express');
+// App de express
+const app = express();
+//puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000;
+
+// path inicial, responderá a la url localhost:3000
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+// Respondiendo texto
+// localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+  res.send('Bienvenidos a launchx');
+})
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+  const explorer = {name: "Explorer", msg: "Hello"}
+  res.send(explorer);
+})
+
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/carlos
+// req.params = {"explorerName": "carlos"}
+app.get('/explorers/:explorerName', (req, res) => {
+  res.send(req.params)
+})
+
+// con esto inicializamos esta app
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
